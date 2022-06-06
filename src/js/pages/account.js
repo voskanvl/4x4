777c1767bc1784d0a1aea681ca578a56f5de8e38
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-06-06 12:38:24 +0300

    account done

--- src/js/pages/account.js
+++ src/js/pages/account.js
@@ -11,6 +11,17 @@ if (document.readyState === "loading") {
 function start() {
     //--- CLICK BigBUTTON
     const bigButton = document.querySelector(".big-button");
+    const catalog = document.querySelector(".account__menu .catalog");
     bigButton.click();
     bigButton.addEventListener("click", () => (window.location = "/"));
+    //--- TOOGLE CATALOG__ITEM ---
+    catalog.addEventListener("click", ({ target }) => {
+        const subMenu = target
+            .closest(".catalog__item")
+            .querySelector(".sub-menu__content");
+        console.log("🚀 ~ subMenu", subMenu);
+        if (subMenu) {
+            subMenu.classList.toggle("sub-menu__content_show");
+        }
+    });
 }
