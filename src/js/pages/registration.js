98b33e5f30c4b339b5c498b63a7d27daa161251c
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-07-06 16:51:09 +0300

    change routes on registration

--- src/js/pages/registration.js
+++ src/js/pages/registration.js
@@ -12,4 +12,13 @@ function start() {
     const paymethdCheck = document.querySelector(".paymethod .checkbox");
     //--- CLICK BigBUTTON
     document.querySelector(".big-button").click();
+    const form = document.querySelector("form.registration__form");
+    form.addEventListener("click", ({ target }) => {
+        if (target.tagName === "INPUT") {
+            const untouched = target.getAttribute("untouched");
+            if (untouched) {
+                target.removeAttribute("untouched");
+            }
+        }
+    });
 }
